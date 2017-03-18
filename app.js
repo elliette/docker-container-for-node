f35a67e8201d76117df8a2f10ef4194901378f57
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-03-18 16:55:57 -0400

    all working

--- app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-var models = require('./models');
-var Sandcastle = models.Sandcastle;
-var db = models.db; 
-
-
--- server.js
+++ server.js
@@ -1,45 +1,35 @@
 'use strict';
 var retry = require('retry');
-var pg = require('pg')
+var pg = require('pg');
+var models = require('./models');
+var db = models.db;
+const express = require('express');
+const userApp = require('./userApp');
 
-var operation = retry.operation({retries:3}); 
+var operation = retry.operation({ retries: 3 });
 
 // Constants
 const PORT = 8080;
 
 // App
-
 operation.attempt(function() {
-  var client = new pg.Client()
-  client.connect(function(e) {
-    client.end()
-    if(operation.retry(e)) {
-      return;
-    }
-    if(!e) console.log("Hello Postgres!")
+    var client = new pg.Client()
+    client.connect(function(e) {
+        client.end()
+        if (operation.retry(e)) {
+            return;
+        }
+        if (!e) console.log("Hello Postgres!")
 
+        const server = express();
+        server.use('/', userApp);
 
-    const app = express();
-    app.get('/', function (req, res) {
-    	console.log(req.url);
-    	Sandcastle.create({
-       	 	name: 'jems'
-     	 })
-      .then(() => {
-        res.write('Hello Can you see this???')
-        res.end();
-      })
-      .catch(console.error);
-    });
-
-    db.sync({force: true})
-    .then(() => {
-      app.listen(PORT, () => {
-        console.log(process.env.DATABASE_URL);
-        console.log('Running on http://localhost:3001');
-      });
+        db.sync({ force: true })
+            .then(() => {
+                server.listen(PORT, () => {
+                    console.log(process.env.DATABASE_URL);
+                    console.log('Running on http://localhost:3001');
+                });
+            })
     })
-
-  })
 })
-
